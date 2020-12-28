84532c12e98a482dec40429158ab435c9a3e01bd
Edward Agyeman <edw.agyeman@gmail.com>
2020-12-28 15:21:37 +0100

    Override Inherited Methods

--- JavaScript Algorithms and Data Structures Certification/Object Oriented Programming/main.js
+++ JavaScript Algorithms and Data Structures Certification/Object Oriented Programming/main.js
@@ -37,7 +37,6 @@
 // let dave = new User("dave@hotmaild.de", "Dave");
 // console.log(dave);
 
-
 // Use Inheritance So You Don't Repeat Yourself
 // function Cat(name) {
 //   this.name = name;
@@ -64,18 +63,85 @@
 //   },
 // };
 
+// ************Inherit Behaviors from a Supertype
+// function Animal() {}
 
-// Inherit Behaviors from a Supertype
-function Animal() {}
+// Animal.prototype = {
+//   constructor: Animal,
+//   eat: function () {
+//     console.log("nom nom nom");
+//   },
+// };
 
-Animal.prototype = {
-  constructor: Animal,
-  eat: function () {
-    console.log("nom nom nom");
-  },
-};
+// let duck = Object.create(Animal.prototype); // Change this line
+// let beagle = Object.create(Animal.prototype); // Change this line
+// let dog = new Animal()
 
+// **********Set the Child's Prototype to an Instance of the Parent
+// function Animal() {}
 
-let duck = Object.create(Animal.prototype); // Change this line
-let beagle = Object.create(Animal.prototype); // Change this line
-let dog = new Animal()
+// Animal.prototype = {
+//   constructor: Animal,
+//   eat: function () {
+//     console.log("nom nom nom");
+//   },
+// };
+
+// function Dog(name) {
+//   this.name = name
+// }
+// // Only change code below this line
+// Dog.prototype = Object.create(Animal.prototype)
+
+// let beagle = new Dog();
+// beagle.eat()
+
+// ************Reset an Inherited Constructor Property
+// function Animal() {}
+// function Bird() {}
+// function Dog() {}
+
+// Bird.prototype = Object.create(Animal.prototype);
+// Dog.prototype = Object.create(Animal.prototype);
+
+// // Only change code below this line
+// // Bird.prototype.constructor = Bird
+// Dog.prototype.constructor = Dog
+
+// let duck = new Bird();
+// let beagle = new Dog();
+
+// ********Add Methods After Inheritance
+// function Animal() {}
+// Animal.prototype.eat = function () {
+//   console.log("nom nom nom");
+// };
+
+// function Dog() {}
+// Dog.prototype = Object.create(Animal.prototype)
+// Dog.prototype.bark = function () {
+//   console.log("Woof!")
+// }
+// Dog.prototype.constructor = Dog;
+
+// let beagle = new Dog();
+
+// ********Override Inherited Methods
+// function Bird() {}
+
+// Bird.prototype.fly = function () {
+//   return "I am flying!";
+// };
+
+// function Penguin() {}
+// Penguin.prototype = Object.create(Bird.prototype);
+// Penguin.prototype.constructor = Penguin;
+
+// // Only change code below this line
+// Penguin.prototype.fly = function() {
+//   return "Alas, this is a flightless bird."
+// }
+// // Only change code above this line
+
+// let penguin = new Penguin();
+// console.log(penguin.fly());
