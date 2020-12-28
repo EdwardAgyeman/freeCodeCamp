e3beef3a68c9ef494e608617aa80030884aa22dc
Edward Agyeman <edw.agyeman@gmail.com>
2020-12-28 19:09:46 +0100

    IIFE Module

--- JavaScript Algorithms and Data Structures Certification/Object Oriented Programming/main.js
+++ JavaScript Algorithms and Data Structures Certification/Object Oriented Programming/main.js
@@ -146,24 +146,54 @@
 // let penguin = new Penguin();
 // console.log(penguin.fly());
 
-
 // ******Use a Mixin to Add Common Behavior Between Unrelated Objects
-let bird = {
-  name: "Donald",
-  numLegs: 2,
-};
+// let bird = {
+//   name: "Donald",
+//   numLegs: 2,
+// };
 
-let boat = {
-  name: "Warrior",
-  type: "race-boat",
-};
+// let boat = {
+//   name: "Warrior",
+//   type: "race-boat",
+// };
 
-let glideMixin = function(obj) {
-  obj.glide = function() {
-    console.log("GLIIIIDE")
-  }
-}
+// let glideMixin = function(obj) {
+//   obj.glide = function() {
+//     console.log("GLIIIIDE")
+//   }
+// }
 
-glideMixin(bird)
-glideMixin(boat)
+// glideMixin(bird)
+// glideMixin(boat)
 
+// ******Use Closure to Protect Properties Within an Object from Being Modified Externally
+// function Bird() {
+//   let weight = 15;
+//   this.getWeight = function () {
+//     return weight;
+//   };
+// }
+
+// let birdy = new Bird();
+// console.log(birdy.getWeight());
+
+// *********Understand the Immediately Invoked Function Expression (IIFE)
+// (function () {
+//   console.log("A cozy nest is ready");
+// })();
+
+// *****Use an IIFE to Create a Module
+let funModule = (function () {
+  return {
+    isCuteMixin: function (obj) {
+      obj.isCute = function () {
+        return true;
+      };
+    },
+    singMixin: function (obj) {
+      obj.sing = function () {
+        console.log("Singing to an awesome tune");
+      };
+    },
+  };
+})()
